e7e252dda3932f29edacc1d4b2cf4f80057842a3
S545515 <charanghanta.1999@gmail.com>
2021-11-11 01:04:37 -0600

    updated

--- controllers/iphone.js
+++ controllers/iphone.js
@@ -30,10 +30,27 @@ exports.iphone_view_all_Page = async function (req, res) {
 exports.iphone_detail = function(req, res) {
  res.send('NOT IMPLEMENTED: iphone detail: ' + req.params.id);
 };
-// Handle iphone create on POST.
-exports.iphone_create_post = function(req, res) {
- res.send('NOT IMPLEMENTED: iphone create POST');
+// Handle iphone create on POST. 
+exports.iphone_create_post = async function (req, res) {
+    console.log(req.body)
+    let document = new iphone();
+    // We are looking for a body, since POST does not have query parameters.
+    // Even though bodies can be in many different formats, we will be picky
+    // and require that it be a json object
+    // {"color":"silver", "cost":850, "model":"14"}
+    document.color = req.body.color; 
+    document.cost = req.body.cost; 
+    document.model = req.body.model;
+    try {
+        let result = await document.save();
+        res.send(result);
+    }
+    catch (err) {
+        res.status(500);
+        res.send(`{"error": ${err}}`);
+    }
 };
+
 // Handle iphone delete form on DELETE.
 exports.iphone_delete = function(req, res) {
  res.send('NOT IMPLEMENTED: iphone delete DELETE ' + req.params.id);
